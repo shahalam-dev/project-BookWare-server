70fecef4f13b63fe5dece5e03b8c558150e5e2b4
shahalam sharif <shahalam.shareef@gmail.com>
2022-05-09 23:53:19 +0600

    insert a data api created

---
+++ _list.js
@@ -0,0 +1,90 @@
+[
+    {
+          "title": "Smarter Faster Better: The Transformative Power of Real Productivity",
+          "author": "Charles Duhigg",
+          "coverPhoto": "https://m.media-amazon.com/images/I/51teWjeZGsL.jpg",
+          "suppler": "Random House",
+          "price": "2041",
+          "quantity": "21"
+    },
+    {
+          "title": "You Can Heal Your Life",
+          "author": "Louise Hay",
+          "coverPhoto": "https://m.media-amazon.com/images/I/516HD6n7UoS.jpg",
+          "suppler": "Hay House India",
+          "price": "899",
+          "quantity": "46"
+    },
+    {
+          "title": "Noise",
+          "author": "Daniel Kahneman , Olivier Sibony , Cass R. Sunstein",
+          "coverPhoto": "https://m.media-amazon.com/images/I/41ahUW55KJS.jpg",
+          "suppler": "William Collins",
+          "price": "1258",
+          "quantity": "13"
+    },
+    {
+          "title": "The Power Of Positive Thinking",
+          "author": "Dr. Norman Vincent Peale",
+          "coverPhoto": "https://m.media-amazon.com/images/I/517G3UytsyL.jpg",
+          "suppler": "Vermilion London",
+          "price": "358",
+          "quantity": "56"
+    },
+    {
+          "title": "The 7 Habits Of Highly Effective People",
+          "author": "Stephen R. Covey",
+          "coverPhoto": "https://m.media-amazon.com/images/I/51OuvCFwyZL.jpg",
+          "suppler": "Simon & Schuster",
+          "price": "1078",
+          "quantity": "87"
+    },
+    {
+          "title": "The Power of your Subconscious Mind",
+          "author": "Joseph Murphy",
+          "coverPhoto": "https://m.media-amazon.com/images/I/5187wPHcPgL.jpg",
+          "suppler": "Amazing Reads",
+          "price": "405",
+          "quantity": "58"
+    },
+    {
+          "title": "Who Moved My Cheese?",
+          "author": "Spencer Johnson",
+          "coverPhoto": "https://m.media-amazon.com/images/I/51om3WtZyWL.jpg",
+          "suppler": "Amazing Reads",
+          "price": "358",
+          "quantity": "78"
+    },
+    {
+          "title": "Think And Grow Rich",
+          "author": "Napoleon Hill",
+          "coverPhoto": "https://m.media-amazon.com/images/I/41CJWmxundL._SY346_.jpg",
+          "suppler": "Vermilion London",
+          "price": "450",
+          "quantity": "18"
+    },
+    {
+          "title": "The Art Of Thinking Clearly",
+          "author": "Rolf Dobelli",
+          "coverPhoto": "https://images-na.ssl-images-amazon.com/images/I/71X49rkdj1L.jpg",
+          "suppler": "Sceptre (India)",
+          "price": "718",
+          "quantity": "58"
+    },
+    {
+          "title": "Mindset Changing the way you think to fulfil your potential",
+          "author": "Carol S Dweck",
+          "coverPhoto": "https://images-na.ssl-images-amazon.com/images/I/61yZA6ehN8L.jpg",
+          "suppler": "Robinson",
+          "price": "718",
+          "quantity": "32"
+    },
+    {
+          "title": "The High-performance Entrepreneur",
+          "author": "Subroto Bagchi",
+          "coverPhoto": "https://images-na.ssl-images-amazon.com/images/I/61SRpLu6T3S.jpg",
+          "suppler": "Penguin group",
+          "price": "630",
+          "quantity": "54"
+    }
+]
--- index.js
+++ index.js
@@ -2,48 +2,78 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
-
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 // use middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
+function verifyJWT(req, res, next){
+      const authHeader = req.headers.authorization;
+      if(!authHeader){
+        return res.status(401).send({message: 'unautorized access'});
+      }
+      const token = authHeader.split(' ')[1];
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if(err){
+          return res.status(403).send({message: 'Forbidden access'})
+        }
+        req.decoded = decoded;
+      })
+      
+      next();
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hxuch.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 
-// const bookData = {
-//   title: "The Power of Habit",
-//   author: "Charles Duhigg",
-//   category: "Self-help and meditation",
-//   suppler: "RH Books",
-//   price: "1078",
-//   quantity: "26"
-// }
 
 async function run() {
   try {
     await client.connect();
     const collection = client.db("warehouse").collection("books-stock");
-    // create a document to insert
     
-    const result = await collection.insertOne(bookData);
-    console.log(`A book was inserted with the _id: ${result.insertedId}`);
+    // Auth
+    app.post('/login', async(req, res) => {
+        const user = req.body;
+        const accessToken = jwt.sign(user, process.env.JWT_SECRET, {
+          expiresIn: '1d'
+        });
+        res.send({accessToken});
+    })
+
+   
+    // insert a data
+
+    app.post("/book", async(req, res) => {
+      const bookData = {
+        bookTitle: req.body.bookTitle,
+        authorName: req.body.authorName,
+        photoUrl: req.body.photoUrl,
+        publisher: req.body.publisher,
+        stockQuantity: parseInt(req.body.stockQuantity),
+        bookPrice: parseInt(req.body.bookPrice),
+        inventoryManager: req.body.inventoryManager
+      }
+      const result = await collection.insertOne(bookData);
+      res.json(result)
+      console.log(`A book was inserted with the _id: ${result.insertedId}`);
+    })
+
+
+
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
-
 
 app.get("/", (req, res) => {
   res.send("hello form server");
